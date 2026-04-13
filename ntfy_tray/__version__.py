5f316994fedaec7dd691ae6025c3208d5d9e9f1c
TuruncuBilisim <turuncubilisim@turuncubilisim-MacBook-Pro.local>
2026-04-13 03:28:35 +0300

    Bump version to 1.3.0

--- ntfy_tray/__version__.py
+++ ntfy_tray/__version__.py
@@ -3,4 +3,4 @@ __description__ = (
     "A tray notification application for receiving messages from a ntfy server."
 )
 __url__ = "https://github.com/selcuksunar/nfty-tray-app"
-__version__ = "1.2.0"
+__version__ = "1.3.0"
